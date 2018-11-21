29b18df16a5647518974da916cebfe80827e9d84
Silveira, Rodrigo <Rodrigo.Silveira@dematic.com>
2018-11-21 04:27:43 -0800

    03_Output_Management Output management changes

--- 03_Output_Management/src/index.js
+++ 03_Output_Management/src/index.js
@@ -20,23 +20,18 @@
  */
 
 import _ from 'lodash';
-import './style.css';
-import Icon from './rafiki.jpg';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
 	let element = document.createElement('div');
+	let btn = document.createElement('button');
 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-	element.classList.add('hello');
 
-	// Add the image to our existing div.
-	var myIcon = new Image();
-	myIcon.src = Icon;
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
 
-	element.appendChild(myIcon);
-
-	console.log(Data);
+	element.appendChild(btn);
 
 	return element;
 }
